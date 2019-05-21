f34b6922d1bd5a8591a17ae828e4c785f2370293
Daniel Berg Frederiksen <danielbergfrederiksen1989@gmail.com>
2019-05-21 20:54:12 +0200

    awd

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
     // mongodb: 'mongodb+srv://FredeSloth:Fr3d3r1k!@jokeservice-koi3c.mongodb.net/test?retryWrites=true',
-    mongodb: 'mongodb+srv://fredesloth:Frederik!@fredeslothcluster-ychnz.mongodb.net/test?retryWrites=true',
+    mongodb: 'mongodb+srv://FredeSloth:Frederik!@jokeservice-koi3c.mongodb.net/test?retryWrites=true',
+    
     localPort: 8080,
     jokeRegistry: 'https://krdo-joke-registry.herokuapp.com',
     heroku: 'https://jokeGalore.herokuapp.com/'
