741f85de8b48f0a1b1ad13b2e32e928228e94f9c
Daniel Berg <danielbergfrederiksen1989@gmail.com>
2019-05-22 10:55:20 +0200

    lort

--- public/script.js
+++ public/script.js
@@ -2,138 +2,140 @@ const jokeContainer = document.getElementById('jokes');
 const btnPost = document.getElementById('btnPost');
 const serviceContainer = document.getElementById('services');
 const newJokeContainer = document.getElementById('newJoke');
+const btnDeleteService = document.getElementById('btnDeleteService');
+const btnCreateService = document.getElementById('btnCreateService');
 const maxJokes = 50;
-
 
 update();
 
+// makes the setup of the html site and calls getServices()
 async function update() {
     jokeContainer.innerHTML = '';
     serviceContainer.innerHTML = '';
     for (let input of document.querySelectorAll('input')) input.value = '';
-    servicesGet();
+    getServices();
 }
 
-function jokesUpdate() {
+// Updates the HTML when selecting another service in the dropdown
+function updateJokes() {
     jokeContainer.innerHTML = '';
+    document.getElementById('postJokeContainer').style.display = 'none';
+    document.getElementById('serviceAddress').innerText = '';
     for (let input of document.querySelectorAll('input')) input.value = '';
-    if (serviceContainer.value != '0') {
-        jokesGet(serviceContainer.value);
+    if (serviceContainer.value != '0' && serviceContainer.value) {
+        getJokes(serviceContainer.value);
     }
 }
 
-async function jokesGet(id) {
-    jokeContainer.innerHTML = 'Loading...';
-    let template;
-    let response;
- 
-    if (!id || serviceContainer.options[serviceContainer.selectedIndex].id === 'https://jokegalore.herokuapp.com/') {
-        response = await fetch('/api/jokes');
-        template = await fetch('/joke.hbs');
-    } else {
-        response = await fetch('/api/otherjokes/' + id);
-        template = await fetch('/otherJoke.hbs');
-    }
-
-    console.log(response);
-
+// gets all jokes from a service through it's id, and puts the 50 latest jokes into the html containter through a handlebars template
+async function getJokes(id) {
+    jokeContainer.innerHTML = '<span id="statusText">Loading latest 50 jokes from: '
+    +  serviceContainer.options[serviceContainer.selectedIndex].id
+    + '</span><img class="svg" alt="loading.gif" src="loading.svg">';
     try {
-        const templateText = await template.text();
-        const jokes = await response.json();
-
-        console.log(jokes);
-
+        const [template, response] = await Promise.all([fetch('/joke.hbs'), fetch('/api/otherjokes/' + id)]);
+        if (response.status >= 400 || !response) {
+            throw new Error('No JSON to fetch');
+        }
+        const [templateText, jokes] = await Promise.all([template.text(), response.json()]);
         const compiledTemplate = Handlebars.compile(templateText);
-
-        console.log(jokes.length);
 
         let jokesHTML = '';
 
-        jokes.forEach(joke => {
+        if (jokes.length > maxJokes) {
+            for (let i = jokes.length-1; i >= jokes.length-maxJokes; i--) {
+                jokesHTML += compiledTemplate({
+                    id: jokes[i]._id,
+                    setup: jokes[i].setup,
+                    punchline: jokes[i].punchline
+                });
+            };
+        } else {
+            for (let i = jokes.length-1; i >= 0; i--) {
+                jokesHTML += compiledTemplate({
+                    id: jokes[i]._id,
+                    setup: jokes[i].setup,
+                    punchline: jokes[i].punchline
+                });
+            }
+        }
+       
+        jokeContainer.innerHTML = jokesHTML;
+        document.getElementById('postJokeContainer').style.display = 'block';
+        document.getElementById('serviceAddress').innerText = serviceContainer.options[serviceContainer.selectedIndex].id;
+
+       /* jokes.forEach(joke => {
             jokesHTML += compiledTemplate({
                 id: joke._id,
                 setup: joke.setup,
                 punchline: joke.punchline,
             });
-        });
-        jokeContainer.innerHTML = jokesHTML;
+        });*/
     } catch (err) {
         console.log(err);
-        jokeContainer.innerHTML = 'Failed to fetch jokes from service';
+        jokeContainer.innerHTML = '<span id="statusText">Failed to fetch jokes from: <a href="' 
+        + serviceContainer.options[serviceContainer.selectedIndex].id + '">' 
+        + serviceContainer.options[serviceContainer.selectedIndex].id +'</a></span>';
     }
 }
 
-async function servicesGet() {
-    const [template, response] = await Promise.all([fetch('/service.hbs'), fetch('/api/othersites')]);
+// gets all services and puts them into the html element with id services through a handlebars template
+async function getServices() {
+    try {
+        const [template, response] = await Promise.all([fetch('/service.hbs'), fetch('/api/othersites')]);
+        if (response.status >= 400 || !response) {
+            throw new Error('Filed to fetch');
+        }
     const [templateText, services] = await Promise.all([template.text(), response.json()]);
-
+    const regex = /^(https:|http:|www\.)\S*/;
     const compiledTemplate = Handlebars.compile(templateText);
     let HTML = '<option value="0" disabled selected>Select a site</option>';
     services.forEach(service => {
-        HTML += compiledTemplate({
-            id: service._id,
-            name: service.name,
-            address: service.address,
-            secret: service.secret
-        });
+        if(regex.test(service.address)) {
+            HTML += compiledTemplate({
+                id: service._id,
+                name: service.name,
+                address: service.address,
+                secret: service.secret
+            });
+        }
     });
-    serviceContainer.innerHTML = HTML;
+        serviceContainer.innerHTML = HTML;
+    } catch (err) {
+        console.log(err);
+    }  
 }
 
-// Not yet implemented
+// delete joke with id from selected service
 async function deleteJoke(id) {
     try {
-        const response = await fetch('/api/jokes/' + id, {
+        const response = await fetch('/api/othersite/jokes/' + serviceContainer.value + '/' + id, {
             method: 'DELETE',
             headers: {'Content-Type': 'application/json'}
         })
+        if (response.status >= 400 || !response) {
+            throw new Error('Failed to fetch');
+        }
         const json = await response.json();
         console.log(`Resultat: %o`, json);
-        jokesUpdate();
+        updateJokes();
     } catch (err) {
         console.log(err);
     }
-   
 }
 
-// Not yet implemented
+// edit a joke with a specific id from the current chosen site
 async function editJoke(id) {
     const setup = document.getElementById('setup-' + id).innerText;
     const punchline = document.getElementById('punchline-' + id).innerText;
     if (setup.length > 0 && punchline.length > 0) {
-        const msg = {
-            setup: setup,
-            punchline: punchline
-        };
+        const msg = {setup: setup, punchline: punchline};
         try {
-            const response = await fetch('/api/jokes/' + id, {
+            const response = await fetch('/api/othersite/jokes/' + serviceContainer.value + '/' + id, {
                 method: 'PUT',
                 body: JSON.stringify(msg),
                 headers: {'Content-Type': 'application/json'}
             })
-            const json = await response.json();
-            console.log(`Result: %o`, json);
-            alert('Your joke has been saved');
-            jokesUpdate();
-        } catch (err) {
-            alert('Sorry, something went wrong: ', err);
-            console.log(err);
-        }
-    }
-}
-
-// when the btnPost button element is clicked a new joke is posted to the currently selected service and the jokes are being updated
-/*btnPost.onclick = async () => {
-    let setup = document.getElementById('setup').value;
-    let punchline = document.getElementById('punchline').value;
-    if (setup.length > 0 && punchline.length > 0) {
-        const msg = {setup: setup, punchline: punchline};
-        try {
-            const response = await fetch('/api/jokes/' + serviceContainer.value, {
-                method: "POST",
-                body: JSON.stringify(msg),
-                headers: {'Content-Type': 'application/json'}
-            });
             if (response.status >= 400 || !response) {
                 throw new Error('Failed to fetch');
             }
@@ -145,21 +147,66 @@ async function editJoke(id) {
             console.log(err);
         }
     }
-};*/
+}
 
-/* btnCreateService.onclick = async () => {
-    const data = {name: 'Joke Galore', address: 'https://jokegalore.herokuapp.com/', secret: 'jokeGalore'};
+// when the btnPost button element is clicked a new joke is posted to the currently selected service and the jokes are being updated
+btnPost.onclick = async () => {
+    let setup = document.getElementById('setup').value;
+    let punchline = document.getElementById('punchline').value
+    if (setup.length > 0 && punchline.length > 0) {
+        const msg = {setup: setup, punchline: punchline};
+        try {
+            const response = await fetch('/api/othersite/jokes/' + serviceContainer.value, {
+                method: "POST",
+                body: JSON.stringify(msg),
+                headers: {'Content-Type': 'application/json'}
+            })
+            if (response.status >= 400 || !response) {
+                throw new Error('Failed to fetch');
+            }
+            const json = await response.json();
+            console.log(`Resultat: %o`, json);
+            updateJokes();
+        } catch (err) {
+            alert('Noget gik galt: ', err);
+            console.log(err);
+        }
+    }
+}
+
+// when the btnDeleteService button element is clicked our service is deleted from the registry
+btnDeleteService.onclick = async () => {
+    const data = {address: 'https://JokeGalore.herokuapp.com/', secret: 'daddy'};
+    try {
+        const service = await fetch('/api/services', {
+            method: "DELETE",
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        if (response.status >= 400 || !response) {
+            throw new Error('Failed to fetch');
+        }
+        const json = await service.json();
+        console.log(`Resultat: %o`, json);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// when the btnCreateService button element is clicked our service is posted to the registry
+btnCreateService.onclick = async () => {
+    const data = {name: 'Dad jokes', address: 'https://JokeGalore.herokuapp.com/', secret: 'daddy'};
     try {
         const service = await fetch('https://krdo-joke-registry.herokuapp.com/api/services', {
             method: "POST",
             body: JSON.stringify(data),
             headers: { 'Content-Type': 'application/json' }
         })
-        if (response.status >= 400 || !response){
-            throw new Error("Fetch failed");
+        if (response.status >= 400 || !response) {
+            throw new Error('Failed to fetch');
         }
         console.log(service);
     } catch (err) {
         console.log(err);
     }
-}*/
+}
