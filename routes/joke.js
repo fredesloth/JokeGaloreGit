12c94345bca7e8ac9ab25287cfbd0db1b03890f4
Daniel Berg <danielbergfrederiksen1989@gmail.com>
2019-05-22 10:44:35 +0200

    dwa

--- routes/joke.js
+++ routes/joke.js
@@ -17,7 +17,7 @@ router
             });
     })
 
-    .post('/jokes', function (req, res) {
+    .post('/', function (req, res) {
         Joke.create(req.body);
           res.send({
               type: 'POST',
