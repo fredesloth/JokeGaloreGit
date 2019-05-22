e99b87a163d6bb980f4e1c1e650bc5ac23e991b6
Daniel Berg <danielbergfrederiksen1989@gmail.com>
2019-05-22 10:55:20 +0200

    Revert "lort"
    This reverts commit 741f85de8b48f0a1b1ad13b2e32e928228e94f9c.

--- routes/joke.js
+++ routes/joke.js
@@ -2,11 +2,13 @@ const controller = require("../controllers/controller");
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
+const Joke = require('../models/joke');
+
 
 router
+
     .get('/jokes', (req, res) => {
-        // Get own jokes
-        controller.getJokes()
+        controller.jokesGet()
             .then(val => res.json(val))
             .catch(err => {
                 console.error("Error: " + err);
@@ -14,104 +16,62 @@ router
                 res.status(500).send(err);
             });
     })
-    .post('/jokes', (req, res) => {
-        // Create new joke to own db
-        const {setup, punchline} = req.body;
-        controller.createJoke(setup, punchline)
-            .then(result => res.json({message: 'Joke saved!', joke: result}))
-            .catch(err => {
-                console.error("Error: " + err);
-                if (err.stack) console.error(err.stack);
-                res.status(500).send(err);
-            });
-    })
+
+    .post('/', function (req, res) {
+        Joke.create(req.body);
+          res.send({
+              type: 'POST',
+              setup: req.body.setup,
+              punchline: req.body.punchline
+          })
+          })
+
     .get('/jokes/:id', (req, res) => {
-        // Get specific own joke
-        controller.getJoke(req.params.id)
-        .then(val => res.json(val))
-        .catch(err => {
-            console.log("Error: " + err);
-            if(err.stack) console.error(err.stack);
-            //res.sendStatus(404); Not found!
-            res.status(500).send(err);
-        });
+        controller.jokeGet(req.params.id)
+            .then(val => res.json(val))
+            .catch(err => {
+                console.log("Error: " + err);
+                if(err.stack) console.error(err.stack);
+                //res.sendStatus(404); Not found!
+                res.status(500).send(err);
+            });
     })
+
     .put('/jokes/:id', (req, res) => {
-        // Edit own joke
         const {setup, punchline} = req.body;
-        controller.editJoke(req.params.id, setup, punchline)
-        .then(result => res.json({message: 'Joke edited!', joke: result}))
+        controller.jokeEdit(req.params.id, setup, punchline)
+            .then(result => res.json({message: 'Joke edited!', joke: result}))
             .catch(err => {
                 console.error("Error: " + err);
                 if (err.stack) console.error(err.stack);
                 res.status(500).send(err);
             });
     })
+
     .get('/othersites', (req, res) => {
-        // Get other jokeservices
-        controller.getOthersites()
-        .then(result => res.json(result))
-        .catch(err => {
-            console.error("Error: " + err);
-            if (err.stack) console.error(err.stack);
-            res.status(500).send(err);
-        });
-    })
-    .get('/otherjokes/:site', (req, res) => {
-        // Get jokes from specific service
-        controller.getOtherSiteJokes(req.params.site)
-        .then(result => res.json(result))
-        .catch(err => {
-            console.error("Error: " + err);
-            if (err.stack) console.error(err.stack);
-            res.status(500).send(err);
-        });
-    })
-    .delete('/jokes/:id', (req, res) => {
-        controller.deleteJoke(req.params.id)
-        .then(result => res.json({message: 'Joke deleted!', joke: result}))
-        .catch(err => {
-            console.error("Error: " + err);
-            if (err.stack) console.error(err.stack);
-            res.status(500).send(err);
-        });
-    })
-    .delete('/services', (req, res) => {
-        const {address, secret} = req.body;
-        controller.deleteService(address, secret)
-        .then(result => res.json({message: 'Service deleted!', service: result}))
-        .catch(err => {
-            console.error("Error: " + err);
-            if (err.stack) console.error(err.stack);
-            res.status(500).send(err);
-        });
-    })
-    .delete('/othersite/jokes/:siteid/:id', (req, res) => {
-        // Delete a joke from a site
-        controller.deleteOtherSiteJoke(req.params.siteid, req.params.id)
-        .then(result => res.json({message: 'Joke deleted!', joke: result}))
-        .catch(err => {
-            console.error("Error: " + err);
-            if (err.stack) console.error(err.stack);
-            res.status(500).send(err);
-        });
-    })
-    .put('/othersite/jokes/:siteid/:id', (req, res) => {
-        // Edit a joke from a site
-        const {setup, punchline} = req.body;
-        controller.editOtherSiteJoke(req.params.siteid, req.params.id, setup, punchline)
-        .then(result => res.json({message: 'Joke edited!', joke: result}))
+
+        controller.otherSitesGet()
+            .then(result => res.json(result))
             .catch(err => {
                 console.error("Error: " + err);
                 if (err.stack) console.error(err.stack);
                 res.status(500).send(err);
             });
     })
-    .post('/othersite/jokes/:siteid', (req, res) => {
-        // Post a joke to a site
-        const {setup, punchline} = req.body;
-        controller.postJokeToSite(req.params.siteid, setup, punchline)
-        .then(result => res.json({message: 'Joke Posted!', joke: result}))
+
+    .get('/otherjokes/:site', (req, res) => {
+        controller.otherSiteJokesGet(req.params.site)
+            .then(result => res.json(result))
+            .catch(err => {
+                console.error("Error: " + err);
+                if (err.stack) console.error(err.stack);
+                res.status(500).send(err);
+            });
+    })
+
+    .delete('/jokes/:id', (req, res) => {
+        controller.jokeDelete(req.params.id)
+            .then(result => res.json({message: 'Joke deleted!', joke: result}))
             .catch(err => {
                 console.error("Error: " + err);
                 if (err.stack) console.error(err.stack);
